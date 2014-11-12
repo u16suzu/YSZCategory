0d16a6e192769e0ab9519d584c728f42bf3d147d
u16suzu <u16suzu@gmail.com>
2014-11-12 12:00:39 +0900

    replace duplicated methods

--- Classes/ios/NSDate+Utilities.m
+++ Classes/ios/NSDate+Utilities.m
@@ -5,26 +5,30 @@
 
 + (NSDate*)createDateWithMonthAndDay:(NSString*)monthDayString{
     NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitDay | NSCalendarUnitMonth | NSCalendarUnitYear fromDate:[NSDate date]];
-    
+
     NSInteger year = [components year];
-    
+
     NSString*dateStr = [NSString stringWithFormat:@"%ld_%@", (long)year, monthDayString];
-    
+
     NSDateFormatter *df = [NSDateFormatter new];
     df.locale = [[NSLocale alloc]initWithLocaleIdentifier:@"ja_JP"];
     df.timeZone = [NSTimeZone timeZoneWithName:@"JST"];
     df.dateFormat = @"yyyy_M_d";
-    
+
     return [df dateFromString:dateStr];
 }
 
 - (BOOL)isSameDateWith:(NSDate*)aDate{
 
-    NSDateComponents *otherDay = [[NSCalendar currentCalendar] components:NSEraCalendarUnit|NSYearCalendarUnit|NSMonthCalendarUnit|NSDayCalendarUnit fromDate:aDate];
-    
-    
-    NSDateComponents *selfDate = [[NSCalendar currentCalendar] components:NSEraCalendarUnit|NSYearCalendarUnit|NSMonthCalendarUnit|NSDayCalendarUnit fromDate:self];
-    
+    NSDateComponents *otherDay =
+    [[NSCalendar currentCalendar] components: NSCalendarUnitEra | NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay
+                                    fromDate:aDate];
+
+
+    NSDateComponents *selfDate =
+    [[NSCalendar currentCalendar] components: NSCalendarUnitEra | NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay
+                                    fromDate:self];
+
     if([selfDate day] == [otherDay day] &&
        [selfDate month] == [otherDay month]){
         return YES;
@@ -35,25 +39,30 @@
 
 - (NSString*)stringDateGMT9{
     NSCalendar *calendar = [NSCalendar currentCalendar];
-    NSDateComponents *comps = [calendar components:(NSYearCalendarUnit    | NSMonthCalendarUnit | NSDayCalendarUnit |
-                                                    NSWeekdayCalendarUnit |  NSHourCalendarUnit | NSMinuteCalendarUnit |
-                                                    NSSecondCalendarUnit  )
-                                          fromDate:self];
-    
+    NSDateComponents *comps =
+    [calendar components:(NSCalendarUnitYear   |
+                          NSCalendarUnitMonth  |
+                          NSCalendarUnitDay    |
+                          NSCalendarUnitWeekday|
+                          NSCalendarUnitHour   |
+                          NSCalendarUnitMinute |
+                          NSCalendarUnitSecond  )
+                fromDate:self];
+
     NSInteger month = [comps month];
     NSInteger day = [comps day];
     NSInteger weekday = [comps weekday];
     NSInteger h = [comps hour];
     NSInteger m = [comps minute];
     NSInteger s = [comps second];
-    
+
     NSDateFormatter *df = [NSDateFormatter new];
     df.locale =[[NSLocale alloc] initWithLocaleIdentifier:@"ja_JP"];
     NSString *weekdaySymbol = [[df shortWeekdaySymbols] objectAtIndex:weekday - 1];
     NSString *weekdayString = [NSString stringWithFormat:@"(%@)", weekdaySymbol];
-    
-    return [NSString stringWithFormat:@"%d/%d/%@ %d:%d:%d",
-            month, day, weekdayString, h, m, s];
+
+    return [NSString stringWithFormat:@"%ld/%ld/%@ %ld:%ld:%ld",
+            (long)month, (long)day, weekdayString, (long)h, (long)m, (long)s];
 }
 
 @end
